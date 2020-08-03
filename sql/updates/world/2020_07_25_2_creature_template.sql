3332d785f46e6ee1a7d6eb32bf08b9ff8f652305
ekzobam <ekzobam@gmail.com>
2020-08-03 23:30:37 +0500

    fix sql

--- sql/updates/world/2020_07_25_2_creature_template.sql
+++ sql/updates/world/2020_07_25_2_creature_template.sql
@@ -1,7 +1,7 @@
 UPDATE `creature_template` SET `ScriptName`='npc_skyguard_khatie' WHERE (`entry`='23335');
 UPDATE `creature_template_addon` SET `mount`='21156' WHERE (`entry`='23335');
 DELETE FROM `creature` WHERE (`guid`='44255');
-INSERT INTO `creature` VALUES (44255, 23335, 530, 1, 0, 0, 2548.58, 7331.08, 373.424, 4.02766, 60, 0, 0, 11000, 0, 0, 0);
+INSERT INTO `creature` VALUES (44255, 23335, 530, 1, 0, 0, 2548.58, 7331.08, 373.424, 4.02766, 60, 0, 0, 11000, 0, 0, 0, 0, 0);
 INSERT INTO `script_texts` VALUES (-1580200, 'Woo hoo! More rays for training!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Skyguard Khatie - SAY1');
 INSERT INTO `script_texts` VALUES (-1580201, 'We never have enough of these rays for the Skyguard. You\'re keeping us afloat... literally!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Skyguard Khatie - SAY2');
 INSERT INTO `script_texts` VALUES (-1580202, 'Oh, isn\'t she a beauty! Can\'t wait to get her properly trained and back up in the air!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'Skyguard Khatie - SAY3');
